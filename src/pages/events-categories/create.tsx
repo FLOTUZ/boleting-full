4174c6f9e297cacdd94795e47c4e7152a22e2208
Sarahi Esquivel Pardo <sarahiesquivel45@gmail.com>
2023-06-15 20:57:11 -0500

    REFACTOR: correcting imports

--- src/pages/events-categories/create.tsx
+++ src/pages/events-categories/create.tsx
@@ -1,6 +1,6 @@
 import DesktopLayoutComponent from "@/layouts/desktop-layout-component/desktop-layout.component";
 import CreateCategoryView from "@/modules/events-categories/views/create-category.view";
-import EditCategoryView from "@modules/events-categories/edit-category.view";
+import EditCategoryView from "@/modules/events-categories/views/edit-category.view";
 import { CreateCategories } from "@/routes";
 import React from "react";
 
