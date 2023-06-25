8f8d12b10d387756948ae72fe94989d9f553c9e3
Emmanuel <flotuz10@gmail.com>
2023-06-25 10:47:44 -0600

    REFACTOR: validator

--- src/validations/event.validations.ts
+++ src/validations/event.validations.ts
@@ -23,16 +23,19 @@ export const CreateEventValidator = y.object().shape({
 export const UpdateEventValidator = y.object().shape({
   event_key: y.string(),
   name: y.string(),
-  description: y.string(),
-  event_location: y.string(),
+  description: y.string().max(255),
+  event_location: y.string().max(255),
   event_location_url: y.string().max(1000),
   start_date: y.date(),
   end_date: y.date(),
-  start_time: y.string(),
-  end_time: y.string(),
+  start_time: y.string().max(5),
+  end_time: y.string().max(5),
   re_entry: y.boolean(),
   event_logo_url: y.string().max(1000),
   event_banner_url: y.string().max(1000),
-  sub_categories: y.array(y.number()),
+  sub_categories: y
+    .array(y.number())
+    .min(1, "Select at least one sub category")
+    .of(y.number().required()),
   event_social_media: y.array(y.string().max(1000)),
 });
