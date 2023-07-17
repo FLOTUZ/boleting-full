57b05aa5ad679139c9f7c9edf4fac3e094bf1e14
Sarahi Esquivel <sarahiesquivel45@gmail.com>
2023-07-16 22:56:11 -0600

    ADD: From dashboard to user list

--- src/layouts/desktop-layout-component/user-list.component.tsx
+++ src/layouts/desktop-layout-component/user-list.component.tsx
@@ -1,14 +1,14 @@
 import { Button } from "@chakra-ui/react";
 import { FaUserAlt } from "react-icons/fa";
+import { UsersPath } from "@/routes";
+import { useRouter } from "next/router";
 
 const UsersComponent = () => {
-  const onRedirect = () => {
-    console.log("recibi un click");
-  };
-
+  const router = useRouter();
+  const redirect = () => router.replace(UsersPath);
   return (
     <>
-      <Button onClick={onRedirect}>
+      <Button onClick={redirect}>
         <FaUserAlt />
       </Button>
     </>
