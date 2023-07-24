e7c0dcfadec669697f8074650d1163a3c2bdfa40
Emmanuel <flotuz10@gmail.com>
2023-07-23 22:01:53 -0600

    ADD: Connect show organizations with DB

--- src/modules/organizations/views/show-organizations.view.tsx
+++ src/modules/organizations/views/show-organizations.view.tsx
@@ -1,8 +1,36 @@
+import IntroAnimationComponent from "@/components/animations/intro-animation.component";
+import { Organization, useShowOrganizationsQuery } from "@/gql/generated";
+import { useEffect, useState } from "react";
 import { Box } from "@chakra-ui/react";
 
 const ShowOrganizationsView = () => {
+  const [organizationsList, setOrganizationsList] = useState<Organization[]>(
+    []
+  );
+  const { data, error } = useShowOrganizationsQuery();
+
+  useEffect(() => {
+    if (data) {
+      setOrganizationsList(data.organizations as Organization[]);
+    }
+  }, [data]);
+
+  if (error) {
+    return (
+      <Box m={4}>
+        {error.graphQLErrors.map((error, index) => (
+          <pre key={index}>{error.message}</pre>
+        ))}
+      </Box>
+    );
+  }
+
   return (
-    <Box m={4}>TODO: Aqui se mostraran las organizaciones que se agregaron</Box>
+    <IntroAnimationComponent data={data}>
+      <Box m={4}>
+        <pre>{JSON.stringify(organizationsList, null, 2)}</pre>
+      </Box>
+    </IntroAnimationComponent>
   );
 };
 
