488356857147e42d355780e2577047c755451f78
Emmanuel <flotuz10@gmail.com>
2023-09-18 23:43:31 -0600

    ADD: Create subcategories

--- src/validations/event-categories.validations.ts
+++ src/validations/event-categories.validations.ts
@@ -1,13 +1,13 @@
 import * as y from "yup";
 
 export const CreateEventCategoryValidator = y.object().shape({
-  name: y.string().required(),
-  description: y.string().optional(),
+  name: y.string().max(50).min(3).required(),
+  description: y.string().max(255).optional(),
   sub_categories: y.array().of(y.number()).optional(),
 });
 
 export const UpdateEventCategoryValidator = y.object().shape({
-  name: y.string().optional(),
-  description: y.string().optional(),
+  name: y.string().max(50).min(3).optional(),
+  description: y.string().max(255).optional(),
   sub_categories: y.array().of(y.number()).optional(),
 });
