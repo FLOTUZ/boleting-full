ede64dc9140c5335398ae0274808a0eb6b548903
Sarahi Esquivel <sarahiesquivel45@gmail.com>
2023-09-28 17:56:24 -0600

    ADD: View about payment options

--- src/modules/orders/components/payment-options.component.tsx
+++ src/modules/orders/components/payment-options.component.tsx
@@ -2,8 +2,7 @@ import {
   Box,
   Button,
   Flex,
-  Grid,
-  GridItem,
+  Heading,
   Icon,
   Spacer,
   Text,
@@ -14,41 +13,36 @@ import { BsFillCreditCard2FrontFill } from "react-icons/bs";
 const PaymentOptions = () => {
   return (
     <>
-      <Grid
-        templateColumns="1fr"
-        gap={4}
-        p={"10px"}
-        borderRadius="8px"
-        justifyItems="center"
-        alignItems="center"
-        h="100vh"
-      >
-        <Box w="90%">
-          <GridItem p="4px" placeContent="center">
-            <Flex alignItems="center" width="100%">
+      <Flex w="100%" h="100%" justify="center" align="center">
+        <Flex direction="column" h="auto" w="90%" p="8px" borderRadius="14px">
+          <Heading size="lg" mb="8px">
+            ¿Cómo quieres pagar?
+          </Heading>
+          <Box p="4px">
+            <Flex width="100%">
               <Button w="100%" h="64px">
-                <Text fontWeight={"normal"} fontSize={"md"}>
+                <Text fontWeight="normal" fontSize="md">
                   Tarjeta credito
                 </Text>
                 <Spacer />
                 <Icon as={AiFillCreditCard} ml={2} />
               </Button>
             </Flex>
-          </GridItem>
+          </Box>
 
-          <GridItem p="4px" placeContent="center">
-            <Flex alignItems="center" width="100%">
+          <Box p="4px">
+            <Flex width="100%">
               <Button w="full" h="64px">
-                <Text fontWeight={"normal"} fontSize={"md"}>
+                <Text fontWeight="normal" fontSize="md">
                   Tarjeta debito
                 </Text>
                 <Spacer />
                 <Icon as={BsFillCreditCard2FrontFill} ml={2} />
               </Button>
             </Flex>
-          </GridItem>
-        </Box>
-      </Grid>
+          </Box>
+        </Flex>
+      </Flex>
     </>
   );
 };
--- src/modules/orders/views/show-order.view.tsx
+++ src/modules/orders/views/show-order.view.tsx
@@ -1,25 +1,28 @@
-import { Box, Flex, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
 import ProductDetail from "../components/product-detail.component";
 import PaymentOptions from "../components/payment-options.component";
 
 const ShowOrderView = () => {
   return (
     <>
-      <Grid
-        templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
-        gap={4}
-        alignItems="center"
-      >
-        {/* Payment */}
-        <GridItem>
-          <PaymentOptions />
-        </GridItem>
+      <Box h="100%">
+        <Grid
+          h="100%"
+          mt="10px"
+          templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
+          gap={4}
+        >
+          {/* Payment */}
+          <GridItem h="100%">
+            <PaymentOptions />
+          </GridItem>
 
-        {/* Product Detail */}
-        <GridItem display={{ base: "none", md: "block" }}>
-          <ProductDetail />
-        </GridItem>
-      </Grid>
+          {/* Product Detail */}
+          <GridItem h="100%" display={{ base: "none", md: "block" }}>
+            <ProductDetail />
+          </GridItem>
+        </Grid>
+      </Box>
     </>
   );
 };
