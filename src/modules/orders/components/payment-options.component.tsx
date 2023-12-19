188d5e31204d75198ba6961eb81462217b189d93
Sarahi Esquivel <sarahiesquivel45@gmail.com>
2023-12-18 18:23:19 -0600

    REFACTOR: Full options, one component

--- src/modules/orders/components/payment-options.component.tsx
+++ src/modules/orders/components/payment-options.component.tsx
@@ -1,47 +1,75 @@
 import {
   Box,
   Button,
+  Divider,
   Flex,
+  FormControl,
+  FormLabel,
   Heading,
   Icon,
+  Input,
+  Link,
+  Radio,
+  RadioGroup,
   Spacer,
+  Stack,
   Text,
+  useMediaQuery,
 } from "@chakra-ui/react";
-import { AiFillCreditCard } from "react-icons/ai";
-import { BsFillCreditCard2FrontFill } from "react-icons/bs";
+import { AiOutlineCreditCard } from "react-icons/ai";
+import { BsCashCoin } from "react-icons/bs";
 
 const PaymentOptions = () => {
+  const [isMobile] = useMediaQuery("(max-width: 768px)");
+  const opciones = [
+    {
+      id: 1,
+      value: "opocion1",
+      texto: "Tarjeta de credito o debito",
+      icono: AiOutlineCreditCard,
+      name: "pago",
+    },
+    {
+      id: 2,
+      value: "opocion2",
+      texto: "Pago en efectivo",
+      icono: BsCashCoin,
+      name: "pago",
+    },
+  ];
   return (
     <>
-      <Flex w="100%" h="100%" justify="center" align="center">
-        <Flex direction="column" h="auto" w="90%" p="8px" borderRadius="14px">
-          <Heading size="lg" mb="8px">
-            ¿Cómo quieres pagar?
+      <Flex h={"100%"} p={4} placeItems={"center"} placeContent={"center"}>
+        <Box w={isMobile ? "100%" : "80%"}>
+          {/* Payment Options */}
+          <Heading mb={8} size="lg" fontWeight={"semibold"}>
+            Opciones de pago
           </Heading>
-          <Box p="4px">
-            <Flex width="100%">
-              <Button w="100%" h="64px">
-                <Text fontWeight="normal" fontSize="md">
-                  Tarjeta credito
+          <RadioGroup>
+            {opciones.map((opcion) => (
+              <Flex
+                flexWrap="wrap"
+                border={"1px solid #808080"}
+                key={opcion.id}
+                align="center"
+                p={4}
+                borderRadius="md"
+                mb={2}
+              >
+                <Icon mr={4} as={opcion.icono} boxSize={6} />
+                <Text pl="2" flex="1" fontSize="md">
+                  {opcion.texto}
                 </Text>
-                <Spacer />
-                <Icon as={AiFillCreditCard} ml={2} />
-              </Button>
-            </Flex>
-          </Box>
-
-          <Box p="4px">
-            <Flex width="100%">
-              <Button w="full" h="64px">
-                <Text fontWeight="normal" fontSize="md">
-                  Tarjeta debito
-                </Text>
-                <Spacer />
-                <Icon as={BsFillCreditCard2FrontFill} ml={2} />
-              </Button>
-            </Flex>
-          </Box>
-        </Flex>
+                <Radio
+                  value={opcion.value}
+                  name={opcion.name}
+                  size="md"
+                  m={"0, 4, 0, 4"}
+                />
+              </Flex>
+            ))}
+          </RadioGroup>
+        </Box>
       </Flex>
     </>
   );
