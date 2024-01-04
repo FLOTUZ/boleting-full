e303e5bc8c1c6e0352e77579fb67f43725ba8bdb
Sarahi Esquivel <sarahiesquivel45@gmail.com>
2024-01-04 15:33:18 -0600

    REFACTOR:Rewrite the structure

--- src/modules/orders/components/payment-options.component.tsx
+++ src/modules/orders/components/payment-options.component.tsx
@@ -1,78 +1,42 @@
-import {
-  Box,
-  Button,
-  Divider,
-  Flex,
-  FormControl,
-  FormLabel,
-  Heading,
-  Icon,
-  Input,
-  Link,
-  Radio,
-  RadioGroup,
-  Spacer,
-  Stack,
-  Text,
-  useMediaQuery,
-} from "@chakra-ui/react";
+//This component has the payment options (cards and cash)
+
+import MyCards from "./my-cards.component";
+import CashComponent from "./cash.component";
+import { Flex, Heading } from "@chakra-ui/react";
+
 import { AiOutlineCreditCard } from "react-icons/ai";
 import { BsCashCoin } from "react-icons/bs";
+import ExpandedPanelComponent from "./expanded-panel";
 
-const PaymentOptions = () => {
-  const [isMobile] = useMediaQuery("(max-width: 768px)");
-  const opciones = [
-    {
-      id: 1,
-      value: "opocion1",
-      texto: "Tarjeta de credito o debito",
-      icono: AiOutlineCreditCard,
-      name: "pago",
-    },
-    {
-      id: 2,
-      value: "opocion2",
-      texto: "Pago en efectivo",
-      icono: BsCashCoin,
-      name: "pago",
-    },
-  ];
+const PaymentOptionsComponent = () => {
   return (
     <>
-      <Flex h={"100%"} p={4} placeItems={"center"} placeContent={"center"}>
-        <Box w={isMobile ? "100%" : "80%"}>
-          {/* Payment Options */}
-          <Heading mb={8} size="lg" fontWeight={"semibold"}>
+      <Flex h={"100%"} placeContent={"center"}>
+        <Flex direction={"column"} w={"100%"} alignSelf={"center"}>
+          <Heading
+            m={"16px 0"}
+            size="lg"
+            fontWeight={"semibold"}
+            alignSelf={"center"}
+          >
             Opciones de pago
           </Heading>
-          <RadioGroup>
-            {opciones.map((opcion) => (
-              <Flex
-                flexWrap="wrap"
-                border={"1px solid #808080"}
-                key={opcion.id}
-                align="center"
-                p={4}
-                borderRadius="md"
-                mb={2}
-              >
-                <Icon mr={4} as={opcion.icono} boxSize={6} />
-                <Text pl="2" flex="1" fontSize="md">
-                  {opcion.texto}
-                </Text>
-                <Radio
-                  value={opcion.value}
-                  name={opcion.name}
-                  size="md"
-                  m={"0, 4, 0, 4"}
-                />
-              </Flex>
-            ))}
-          </RadioGroup>
-        </Box>
+
+          <ExpandedPanelComponent
+            icon={AiOutlineCreditCard}
+            title={"Tarjeta de credito o debito"}
+            isDefaultExpanded={false}
+          >
+            <MyCards isVisible={false} />
+          </ExpandedPanelComponent>
+
+          <ExpandedPanelComponent icon={BsCashCoin} title={"Pago en efectivo"}>
+            <CashComponent isVisible={false} />
+          </ExpandedPanelComponent>
+        </Flex>
       </Flex>
     </>
   );
 };
 
-export default PaymentOptions;
+export default PaymentOptionsComponent;
