fb9cf2383e5370535116639e61f5bb04865b87d5
Sarahi Esquivel <sarahiesquivel45@gmail.com>
2024-02-21 18:52:43 -0600

    REFACTOR: Moved the content

--- src/pages/orders/create.tsx
+++ src/pages/orders/create.tsx
@@ -1,55 +1,16 @@
-import Link from "next/link";
-
 import LandingLayout from "@/layouts/landing-layout.component";
-import IntroAnimationComponent from "@/components/animations/intro-animation.component";
-
-import { ShowOrderPath } from "@/routes";
-import { Box, Button, Heading } from "@chakra-ui/react";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { useCreateOpenVenuePaymentLinkLazyQuery } from "@/gql/generated";
+import OrderResumeView from "@/modules/orders/views/order-resume.view";
+import { Box } from "@chakra-ui/react";
 
 const CreateOrderRoute = () => {
-  const router = useRouter();
-  const { eventId, accessTypeId, buyedTicketsCount } = router.query;
-
-  const [CHECK_OPEN_VENUE, { data, loading, error }] =
-    useCreateOpenVenuePaymentLinkLazyQuery({
-      variables: {
-        data: {
-          eventId: Number(eventId),
-          access_typeId: Number(accessTypeId),
-          buyed_access_count: Number(buyedTicketsCount),
-        },
-      },
-      onCompleted: (data) => {
-        router.push(data.createOpenVenueOrder);
-      },
-    });
-
   return (
-    <LandingLayout>
-      <IntroAnimationComponent data={true}>
-        <Heading>Create order</Heading>
-
-        <Link href={ShowOrderPath("test")}>
-          <Box>
-            Evento: {eventId} - Access type: {accessTypeId} - Buyed tickets:{" "}
-            {buyedTicketsCount}
-          </Box>
-        </Link>
-
-        <Button
-          w={"full"}
-          placeContent={"start"}
-          variant="ghost"
-          isLoading={loading}
-          onClick={() => CHECK_OPEN_VENUE()}
-        >
-          Orden de prueba
-        </Button>
-      </IntroAnimationComponent>
-    </LandingLayout>
+    <>
+      <LandingLayout>
+        <Box display={"flex"} justifyContent={"center"}>
+          <OrderResumeView />
+        </Box>
+      </LandingLayout>
+    </>
   );
 };
 
